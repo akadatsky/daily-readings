f81f9dced695ef85f9fa5a7978f0c2e454931f9f
Taras Bobriienko <taras3189@gmail.com>
2023-02-09 12:21:58 +0200

    SaveLocal02 commit

--- lib/local/save_local.dart
+++ lib/local/save_local.dart
@@ -1,7 +1,8 @@
-
 import 'dart:io' as io;
 import 'package:path/path.dart' as p;
 import 'package:sqflite/sqflite.dart' as sqflite;
+import 'package:sqflite/sqflite.dart';
+
 
 
 class SaveLocal {
@@ -12,12 +13,58 @@ class SaveLocal {
   static Database _db;
 
   Future<Database> get db async {
-    if(_db !=null) return _db;
+    if (_db != null) return _db;
     _db = await initDb();
     return _db;
   }
+}
+
+SaveLocal.internal();
+
+initDb() async {
+  io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
+  String path = p.join(documentsDirectory.path, "daily_readings.db");
+  var theDb = await sqflite.openDatabase(path, version: 1, onCreate: _onCreate);
+  return theDb;
+}
+
+void _onCreate(Database db, int version) async {
+  await db.execute(
+      "CREATE TABLE DailyReadings(id INTEGER PRIMARY KEY, title TEXT, body TEXT)");
+}
+
+Future<int> addData(String title, String body) async {
+  var dbClient = await db;
+  int res = await dbClient.insert("DailyReadings", {
+    "title": title,
+    "body": body
+  });
+  return res;
+}
+
+Future<List<Map<String, dynamic>>> getData() async {
+  var dbClient = await db;
+  var result = await dbClient.query("DailyReadings");
+  return result;
 
 }
 
 
-SaveLocal.internal();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
