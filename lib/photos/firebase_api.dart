6623d3b1e327b8237908a8944468c14ee3d7bc20
Taras Bobriienko <taras3189@gmail.com>
2023-02-17 17:42:13 +0200

    SavePhotos02 commit

--- lib/photos/firebase_api.dart
+++ lib/photos/firebase_api.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_storage/firebase_storage.dart';
 
+import '../model/firebase_file.dart';
 
 class FirebaseApi {
   static Future<List<String>> _getDownloadLinks(List<Reference> refs) =>
-      refs.map().toList();
+      Future.wait(refs.map((ref) => ref.getDownloadURL()).toList());
 
   static Future<List<FirebaseFile>> listAll(String path) async {
     final ref = FirebaseStorage.instance.ref(path);
@@ -14,31 +15,27 @@ class FirebaseApi {
     return links
         .asMap()
         .map((index, url) {
-      final ref = result.items[index];
-      final name = ref.name;
-      final file = FirebaseFile(ref: ref, name: name, url: url);
-    })
+          final ref = result.items[index];
+          final name = ref.name;
+          final file = FirebaseFile(ref: ref, name: name, url: url);
 
+          return MapEntry(index, file);
+        })
         .values
         .toList();
-
   }
 
 
 
-
-
-  // static Future<String> uploadFile(String path, File file) async {
-  //   final ref = FirebaseStorage.instance.ref(path);
-  //   final task = ref.putFile(file);
-  //
-  //   final snapshot = await task;
-  //
-  //   final downloadUrl = await snapshot.ref.getDownloadURL();
-  //
-  //   return downloadUrl;
-  // }
-
-
+// static Future<String> uploadFile(String path, File file) async {
+//   final ref = FirebaseStorage.instance.ref(path);
+//   final task = ref.putFile(file);
+//
+//   final snapshot = await task;
+//
+//   final downloadUrl = await snapshot.ref.getDownloadURL();
+//
+//   return downloadUrl;
+// }
 
 }
