98bf0742cac79ccce0cc9db3fff2c3bc9f7df570
Taras Bobriienko <taras3189@gmail.com>
2023-02-21 17:26:01 +0200

    Calendar03 commit

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,10 +1,10 @@
 import 'package:daily_readings/selected_date_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_holo_date_picker/flutter_holo_date_picker.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 
 class Calendar extends StatefulWidget {
   const Calendar({Key? key}) : super(key: key);
@@ -17,6 +17,24 @@ class _CalendarState extends State<Calendar> {
   late SelectedDateProvider _selectedDateProvider;
   DateTime today = DateTime.now();
   String formattedDate = DateFormat.yMMMMd().format(DateTime.now());
+
+  void _selectDate(BuildContext context) async {
+    final DateTime? picked = await DatePicker.showSimpleDatePicker(
+      context,
+      initialDate: _selectedDateProvider.selectedDate,
+      firstDate: DateTime.utc(1900, 10, 11),
+      lastDate: DateTime.utc(2150, 3, 14),
+      dateFormat: "dd-MMM-yyyy",
+      locale: DateTimePickerLocale.en_us,
+      looping: true,
+    );
+    if (picked != null) {
+      setState(() {
+        _selectedDateProvider.selectedDate = picked;
+        formattedDate = DateFormat.yMMMd().format(picked);
+      });
+    }
+  }
 
   @override
   void initState() {
@@ -38,6 +56,12 @@ class _CalendarState extends State<Calendar> {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.calendar),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => _selectDate(context),
+            icon: const Icon(Icons.calendar_today),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
@@ -52,8 +76,8 @@ class _CalendarState extends State<Calendar> {
               selectedDayPredicate: (day) =>
                   isSameDay(day, _selectedDateProvider.selectedDate),
               focusedDay: _selectedDateProvider.selectedDate,
-              firstDay: DateTime.utc(2010, 10, 11),
-              lastDay: DateTime.utc(2100, 3, 14),
+              firstDay: DateTime.utc(1900, 10, 11),
+              lastDay: DateTime.utc(2150, 3, 14),
               onDaySelected: _onDaySelected,
             ),
           ],
