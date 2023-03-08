e8b2383546b70187efc8d67b1bbf28ce4cb741f5
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2023-03-08 15:40:57 +0200

    Add tab tap

--- lib/ui/calendar_pager.dart
+++ lib/ui/calendar_pager.dart
@@ -10,7 +10,7 @@ class CalendarPagerController {
   CalendarPagerController(this.selectedDate);
 }
 
-class CalendarPager extends StatelessWidget {
+class CalendarPager extends StatefulWidget {
   final CalendarPagerController controller;
   final PageBuilder builder;
   final PageListener pageChangedListener;
@@ -23,71 +23,18 @@ class CalendarPager extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: const Color.fromARGB(255, 71, 123, 171),
-          flexibleSpace: TabBar(
-            labelColor: Colors.white,
-            tabs: [
-              Tab(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Icon(Icons.wb_sunny),
-                    const SizedBox(width: 8),
-                    Text(AppLocalizations.of(context)!.morning),
-                  ],
-                ),
-              ),
-              Tab(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Icon(Icons.wb_twighlight),
-                    const SizedBox(width: 8),
-                    Text(AppLocalizations.of(context)!.evening),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        body: CalendarPage(
-          controller: controller,
-          builder: builder,
-          pageChangedListener: pageChangedListener,
-        ),
-      ),
-    );
-  }
+  State<CalendarPager> createState() => _CalendarPagerState();
 }
 
-class CalendarPage extends StatefulWidget {
-  final CalendarPagerController controller;
-  final PageBuilder builder;
-  final PageListener pageChangedListener;
-
-  const CalendarPage({
-    required this.controller,
-    required this.builder,
-    required this.pageChangedListener,
-    super.key,
-  });
-
-  @override
-  State<CalendarPage> createState() => _CalendarPageState();
-}
-
-class _CalendarPageState extends State<CalendarPage> {
+class _CalendarPagerState extends State<CalendarPager>
+    with TickerProviderStateMixin {
   static const int maxInt = 0x7fffffff; // 32-bit
   final PageController _pageController = PageController(
     initialPage: maxInt ~/ 2,
   );
   var currentPage = maxInt ~/ 2;
+
+  late TabController tabController;
 
   bool isMorning = true;
 
@@ -95,39 +42,68 @@ class _CalendarPageState extends State<CalendarPage> {
 
   @override
   void initState() {
-    WidgetsBinding.instance.addPostFrameCallback(
-      (_) {
-        TabController tabController = DefaultTabController.of(context)!;
-        tabController.addListener(() {
-          if (tabController.indexIsChanging) {
-            // print('//===============================');
-            // print(tabController.index);
-            // print(tabController.previousIndex);
-          }
-        });
-      },
-    );
+    tabController = TabController(length: 2, vsync: this);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      controller: _pageController,
-      onPageChanged: _onPageChanged,
-      itemBuilder: (context, position) {
-        var nextDate = widget.controller.selectedDate;
-        if (position > currentPage && isEvening) {
-          nextDate = nextDate.add(const Duration(days: 1));
-        }
-        if (position > currentPage && isMorning) {
-          nextDate = nextDate.subtract(const Duration(days: 1));
-        }
-        return widget.builder.call(
-          widget.controller.selectedDate,
-          isMorning,
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color.fromARGB(255, 71, 123, 171),
+        flexibleSpace: TabBar(
+          controller: tabController,
+          onTap: (page) {
+            if (page == 0 && isEvening) {
+              _pageController.jumpToPage(currentPage - 1);
+            }
+            if (page == 1 && isMorning) {
+              _pageController.jumpToPage(currentPage + 1);
+            }
+          },
+          labelColor: Colors.white,
+          tabs: [
+            Tab(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(Icons.wb_sunny),
+                  const SizedBox(width: 8),
+                  Text(AppLocalizations.of(context)!.morning),
+                ],
+              ),
+            ),
+            Tab(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(Icons.wb_twighlight),
+                  const SizedBox(width: 8),
+                  Text(AppLocalizations.of(context)!.evening),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        itemBuilder: (context, position) {
+          var nextDate = widget.controller.selectedDate;
+          if (position > currentPage && isEvening) {
+            nextDate = nextDate.add(const Duration(days: 1));
+          }
+          if (position > currentPage && isMorning) {
+            nextDate = nextDate.subtract(const Duration(days: 1));
+          }
+          return widget.builder.call(
+            widget.controller.selectedDate,
+            isMorning,
+          );
+        },
+      ),
     );
   }
 
@@ -144,7 +120,7 @@ class _CalendarPageState extends State<CalendarPage> {
     isMorning = !isMorning;
     widget.pageChangedListener.call(widget.controller.selectedDate);
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      DefaultTabController.of(context)?.animateTo(isMorning ? 0 : 1);
+      tabController.animateTo(isMorning ? 0 : 1);
     });
   }
 }
