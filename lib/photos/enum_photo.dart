2eabc75c4310be64f095cc917569f5a247b31126
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2023-03-23 20:43:20 +0200

    Update month image

--- lib/photos/enum_photo.dart
+++ lib/photos/enum_photo.dart
@@ -1,52 +1,24 @@
-import 'package:flutter/material.dart';
+enum MonthAsset {
+  january('Jan'),
+  february('Feb'),
+  march('March'),
+  april('April'),
+  may('May'),
+  june('June'),
+  july('July'),
+  august('Aug'),
+  september('Sept'),
+  october('Oct'),
+  november('Nov'),
+  december('Dec');
 
-enum Month {january, february, march, april, may, june, july, august, september,
-  october, november, december}
+  const MonthAsset(this.name);
 
-extension MonthExtension on Month {
-   String get morningImage => 'assets/Morning/$imageAsset';
-   String get eveningImage => 'assets/Evening/$imageAsset';
-   String get imageAsset =>
-      {
-        Month.january: 'Jan.jpg',
-        Month.february: 'Feb.jpg',
-        Month.march: 'March.jpg',
-        Month.april: 'April.jpg',
-        Month.may: 'May.jpg',
-        Month.june: 'June.jpg',
-        Month.july: 'July.jpg',
-        Month.august: 'Aug.jpg',
-        Month.september: 'Sept.jpg',
-        Month.october: 'Oct.jpg',
-        Month.november: 'Nov.jpg',
-        Month.december: 'Dec.jpg',
+  final String name;
 
-      }[this] ??
-  '';
-}
+  String get morningPath => 'assets/Morning/$name.jpg';
 
-class PhotoStack extends StatelessWidget {
-  const PhotoStack (
-  {Key? key, required this.month, required this.morningDescription}) : super(key: key);
+  String get eveningPath => 'assets/Evening/$name.jpg';
 
-  final Month month;
-  final bool morningDescription;
-
-  bool get hasImage => !morningDescription || month.imageAsset.isEmpty;
-
-  @override
-  Widget build(BuildContext context) => Stack(children: [
-    hasImage
-    ? Image(
-      image: AssetImage(month.imageAsset),
-      width: double.infinity,
-      height: MediaQuery.of(context).size.height * 0.37,
-      fit: BoxFit.fill,
-    )
-        : const SizedBox(),
-    Container(
-
-    )
-  ]);
-
+  String path(bool isMorning) => isMorning ? morningPath : eveningPath;
 }
